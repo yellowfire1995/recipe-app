d9987c604fc9372161ca9571197c381aff14cc30
stephen <mr.stephen.walker@gmail.com>
2026-02-18 16:35:31 -0500

    updated url checker

--- recipe-app/src/utils/isValidUrl.js
+++ recipe-app/src/utils/isValidUrl.js
@@ -1,14 +1,10 @@
 export const isValidUrl = (urlString) => {
-  var urlPattern = new RegExp(
-    "^(https?:\\/\\/)?" + // validate protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
-      "(\\#[-a-z\\d_]*)?$",
-    "i",
-  ); // validate fragment locator
-  return !!urlPattern.test(urlString);
+  try {
+    const url = new URL(urlString);
+    return url.protocol === "http:" || url.protocol === "https:";
+  } catch {
+    return false;
+  }
 };
 
 export const isValidHTML = (textString) => {
