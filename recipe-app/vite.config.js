f94b7d26d02de00c3d501c072fce6da83bc502e8
stephen <mr.stephen.walker@gmail.com>
2026-03-14 14:57:21 -0400

    switch to lightning css minify

--- recipe-app/vite.config.js
+++ recipe-app/vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    cssMinify: "esbuild", // use esbuild instead of lightningcss
-  },
   plugins: [react()],
   server: {
     allowedHosts: ["cbcdev.wfhomelocal.com"],
